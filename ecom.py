9f5eaf24ac185808fbf52d59fd0b28da4f275841
NessDAT <99190959+NessDAT@users.noreply.github.com>
2022-08-08 21:03:24 +0000

    Update ecom.py

--- ecom.py
+++ ecom.py
@@ -118,7 +118,7 @@ if selected == "Churn Prediction":
                 'Gender':[gender],'MaritalStatus':[maritalStatus]}
                 data = pd.DataFrame.from_dict(data)
 
-                loaded_model2 = load_model('Churn/models/Churn_Model2')
+                loaded_model2 = load_model('data/Churn_Model2')
                 pred_data = predict_model(loaded_model2, data=data)
                 pred_data.rename(columns={"Label":"PredictedChurn","Score":"PredictionConfidence"},inplace=True)
 
@@ -165,7 +165,7 @@ if selected == "Churn Prediction":
             if st.button('Submit'):
                 st.markdown("____")
                 st.subheader("E-commerce Churn Prediction")
-                loaded_model = load_model("Churn/models/Churn_Model2")
+                loaded_model = load_model("data/Churn_Model2")
                 pred_df = predict_model(loaded_model, data=df)
                 pred_df.rename(columns={"Label":"PredictedChurn","Score":"PredictionConfidence"},inplace=True)
                 pred_df['Target'] = pred_df["PredictedChurn"]
